474e325f1c97c890e54330bcee54520062d4786e
bevchou <31548243+bevchou@users.noreply.github.com>
2018-11-07 01:44:03 -0500

    works, but the colors suck

--- 2_Asemic_Text/sketch.js
+++ 2_Asemic_Text/sketch.js
@@ -1,54 +1,42 @@
 //GLOBAL VARIABLES
 //current position of last "word"
 let cursorX;
-let cursorY;
 let currentWordWidth;
 let marginSide = 50;
 let marginTop = 50;
 let pageW = 500;
 let pageH = 600;
 let spaceWidth = 10;
-let lineHeight = 35;
+let lineHeight = 55;
+let highlight = 0;
 
 function setup() {
   createCanvas(pageW, pageH);
   strokeWeight(1.75);
+  stroke(188,58,22);
   noFill();
+
 }
 
+// for (let i = 0; i < 7; i++) {
+//   translate(0, i*lineHeight);
+
 function draw() {
-  background(220);
-  //track position of text on canvas
-  cursorX = marginSide;
-  cursorY = marginTop;
-
+  background(168, 166, 196);
   push();
-  //start position
-  translate(marginSide - 35, marginTop);
-  //space available in each row for writing
-  let widthWriteSpace = pageW - 2 * marginSide;
-
-  for (let i = 0; i < 7; i++) {
-    translate(0, i*lineHeight);
-    console.log(i*lineHeight);
-    //write while there's still width space
-    while (cursorX < widthWriteSpace) {
-      makeLine();
-      //track new position
-      //moveCursor is the amount to move the cursor
-      let moveCursor = currentWordWidth + spaceWidth;
-      cursorX += moveCursor;
-      //if the word is too big to fit, push it off the page
-      if (cursorX > widthWriteSpace) {
-        translate(999999999, 0);
-        cursorX += 999999999;
-        break;
-      }
-      translate(moveCursor, 0);
+  for (let i = 0; i < 10; i++) {
+    //track position of text on canvas
+    cursorX = marginSide;
+    //next line (move if after the first line)
+    if (i > 0) {
+      translate(0, lineHeight);
     }
-
+    //draw a row of text
+    makeRow();
   }
   pop();
+
+  //don't loop
   noLoop();
 }
 
@@ -81,6 +69,31 @@ function makeLine() {
   endShape();
   currentWordWidth = totalTime;
 }
+
+function makeRow() {
+  push();
+  //start position
+  translate(marginSide - 35, marginTop);
+  //space available in each row for writing
+  let widthWriteSpace = pageW - 2 * marginSide - 20;
+  //write while there's still width space
+  while (cursorX < widthWriteSpace) {
+    makeLine();
+    //track new position
+    //moveCursor is the amount to move the cursor
+    let moveCursor = currentWordWidth + spaceWidth;
+    cursorX += moveCursor;
+    //if the word is too big to fit, push it off the page
+    if (cursorX > widthWriteSpace) {
+      translate(999999999, 0);
+      cursorX += 999999999;
+      break;
+    }
+    translate(moveCursor, 0);
+  }
+  pop();
+}
+
 
 //switch it up when you click the mouse
 function mousePressed() {
